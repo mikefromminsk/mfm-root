b600235d6fd930b68ae169dcc06a5985f4db2f89
x29a100 <x29a100@mail.ru>
2020-01-22 12:06:19 +0300

    send.php

--- wallet/send.php
+++ wallet/send.php
@@ -2,18 +2,31 @@
 
 include_once "login.php";
 
-$coin = get_required("coin");
-$count = get("count");
-$receiver = get("receiver");
-$description = get("description");
-$redirect = get("redirect");
+$coin_name = get_required("coin");
+$coin_count = get_int("count");
+$receiver_user_login = get("receiver");
+
 $message = "";
-if ($coin != null && $count != null && $receiver != null) {
-    if ($count > 100)
-        $message = "code doesnt exist";
-    else
-        redirect($redirect ?: "wallet", array("token" => $token));
+
+if ($coin_name != null && $coin_count != null && $receiver_user_login != null) {
+    $receiver = selectMap("select * from users where user_login = '$receiver_user_login'");
+    if ($receiver != null) {
+        $coin = selectMap("select * from coins where coin_name = '$coin_name'");
+        if ($coin != null) {
+            $coins = selectList("select * from domain_keys where user_id = " . $user["user_id"] . " and coin_id = " . $coin["coin_id"] . " limit $coin_count");
+            if (sizeof($coins) == $coin_count) {
+                update("update domain_keys set user_id = " . $receiver["user_id"] . " where user_id = " . $user["user_id"] . " limit $coin_count");
+            } else
+                $message = "not enough coins";
+        } else
+            $message = "coin doesnt exist in wallet";
+    } else
+        $message = "receiver doesnt exist";
+
+    if ($message == "")
+        redirect("wallet", array("token" => $token));
 }
+
 ?>
 <html>
 <head>
@@ -24,18 +37,10 @@ if ($coin != null && $count != null && $receiver != null) {
     <table>
         <tr>
             <td>
-                Description:
-            </td>
-            <td>
-                <input type="text" name="description" value="<?=$description?>" />
-            </td>
-        <tr/>
-        <tr>
-            <td>
                 Receiver:
             </td>
             <td>
-                <input type="text" name="receiver" value="<?=$receiver?>" required/>
+                <input type="text" name="receiver" value="<?= $receiver_user_login ?>" required/>
             </td>
         <tr/>
         <tr>
@@ -43,7 +48,7 @@ if ($coin != null && $count != null && $receiver != null) {
                 Coin:
             </td>
             <td>
-                <input type="text" name="coin" value="<?= $coin ?>" required/>
+                <input type="text" name="coin" value="<?= $coin_name ?>" required/>
             </td>
         <tr/>
         <tr>
@@ -51,7 +56,7 @@ if ($coin != null && $count != null && $receiver != null) {
                 Count:
             </td>
             <td>
-                <input type="number" name="count" required/>
+                <input type="number" name="count"  value="<?= $coin_count ?>" required/>
             </td>
         <tr/>
         <tr>
--- wallet/wallet.php
+++ wallet/wallet.php
@@ -5,16 +5,30 @@ include_once "login.php";
 ?>
 <html>
 <head>
+    <script>
+        function copyToClipboard() {
+
+            var copyText = document.getElementById("wallet_addr");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+            copyText.setSelectionRange(0, 0);
+            copyText.blur();
+
+            document.getElementById("copy_button").innerText = 'coped';
+        }
+    </script>
 </head>
 <body>
 <table>
     <tr>
-        <th>Wallet user_login</th>
+        <th>Wallet of user: <?= $user["user_login"] ?></th>
     </tr>
     <tr>
         <td>
-            <input id="wallet_addr" value="123" disabled/>
-            <button onclick="this.innerText = 'coped';">copy</button>
+            <input id="wallet_addr"
+                   value="<?= "http://" . $_SERVER["HTTP_HOST"] . str_replace("wallet.php", "send", $_SERVER["SCRIPT_NAME"]) . "?receiver=" . $user["user_login"] ?>"/>
+            <button id="copy_button" onclick="copyToClipboard()">copy</button>
             <br>
             <?php
             $coins = select("select *, count(*) as 'coin_count' from domain_keys t1"
@@ -25,7 +39,7 @@ include_once "login.php";
                 <br>
                 <a href="send?token=<?= $token ?>&coin=<?= $coin["coin_name"] ?>">
                     <div style="background-color: lightgray;">
-                        <big><b><?=$coin["coin_count"]?></b> <?=$coin["coin_code"]?></big><br>
+                        <big><b><?= $coin["coin_count"] ?></b> <?= $coin["coin_code"] ?></big><br>
                         <small>1 DarkCoin = 0.001 USD</small>
                     </div>
                 </a>
