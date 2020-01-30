9fe0d8eda5a8dbebcde7e36338756a2249cda636
x29a100 <x29a100@mail.ru>
2020-01-30 17:09:02 +0300

    wallet/index.html

--- client/login/controller.js
+++ client/login/controller.js
@@ -1,4 +1,7 @@
 controller("login", function ($scope, $routeParams) {
     $scope.login = "123";
     $scope.password = "123";
+    $scope.loginButton = function () {
+        $scope.open('wallet');
+    }
 })
--- client/wallet/controller.js
+++ client/wallet/controller.js
@@ -1,3 +1,15 @@
+function get(name){
+    return angular.element(document.querySelector(name))[0]
+}
+
 controller("wallet", function ($scope, $routeParams) {
     $scope.message = "12d12d12";
+
+    $scope.next = function () {
+        get("#main-page").scrollLeft += get("#left-fragment").clientWidth;
+    }
+
+    $scope.back = function () {
+        get("#main-page").scrollLeft = 0;
+    }
 })
