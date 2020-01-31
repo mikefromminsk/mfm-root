338540075463e96d3ab1ee6746d5f1f983799f0b
x29a100 <x29a100@mail.ru>
2020-01-31 10:26:42 +0300

    md-ink-ripple

--- client/wallet/controller.js
+++ client/wallet/controller.js
@@ -1,11 +1,16 @@
-function get(name){
-    return angular.element(document.querySelector(name))[0]
-}
-
 controller("wallet", function ($scope, $routeParams) {
-    $scope.message = "12d12d12";
-
-    $scope.page = function (index) {
-        get("#main-page").scrollLeft = window.innerWidth * index;
-    }
+    $scope.walletId = "http://darkwallet.store/user_login";
+    $scope.coins = [
+        {"coin_code": "USD", "coin_count": 65000},
+        {"coin_code": "BTC", "coin_count": 1000},
+        {"coin_code": "FTS", "coin_count": 2400},
+        {"coin_code": "SEF", "coin_count": 10040},
+        {"coin_code": "SIL", "coin_count": 2},
+        {"coin_code": "WGS", "coin_count": 1205},
+        {"coin_code": "VWD", "coin_count": 62125},
+        {"coin_code": "WBWE", "coin_count": 120},
+        {"coin_code": "EGW", "coin_count": 1207},
+        {"coin_code": "BWQET", "coin_count": 120},
+        {"coin_code": "VEQ", "coin_count": 40407},
+    ]
 })
