4254d81cb7255780645776408be6f737242e4813
x29a100 <x29a100@mail.ru>
2020-02-25 17:56:52 +0300

    darkapp/index.html

--- darkapp/controller.js
+++ darkapp/controller.js
@@ -1,4 +1,4 @@
-controller("darkapp", function ($scope, $timeout, $q) {
+controller("darkapp", function ($scope, $timeout, $q, $http) {
 
     $scope.files = [
         {file_name: "New Folder", file_size: 0},
@@ -64,7 +64,8 @@ controller("darkapp", function ($scope, $timeout, $q) {
     ];
 
 
-    $scope.toggleExplorerFragment = false
+    $scope.toggleCreateAppFragment = true
+    $scope.toggleExplorerFragment = true
     $scope.app = null;
 
     $scope.search_request_in_progress = false;
@@ -81,6 +82,32 @@ controller("darkapp", function ($scope, $timeout, $q) {
     }
     updateData();
 
+    $scope.path = function () {
+        return "Path/dir/"
+    }
 
+    $scope.upload = function () {
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = function(e){
+            var file = e.target.files[0];
+            var formData = new FormData();
+            formData.append("token", 123);
+            formData.append("userfile", file);
+            var request = new XMLHttpRequest();
+            request.open("POST", pathToRootDir + "/darknode/file_upload.php");
+            request.send(formData);
+        }
+        input.click();
+    }
+
+
+    $scope.delete = function (file) {
+
+    }
+
+    $scope.uploadLogo = function(){
+
+    }
 
 })
