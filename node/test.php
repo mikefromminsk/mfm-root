75f02a096f08d009b2cb74ee25584af38b550305
x29a100 <x29a100@mail.ru>
2020-03-20 15:27:52 +0300

    test cron

--- node/test.php
+++ node/test.php
@@ -2,12 +2,4 @@
 include_once $_SERVER["DOCUMENT_ROOT"] . "/node/domain_utils.php";
 
 
-$repo_path = $_SERVER["DOCUMENT_ROOT"] . "/search/app.zip";
-$zip = new ZipArchive();
-if ($zip->open($repo_path) == TRUE) {
-    for ($i = 0; $i < $zip->numFiles; $i++) {
-        $file_path = $zip->getNameIndex($i);
-        $file_data = $zip->getFromName($file_path);
-        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/$domain_name/$file_path", $file_data);
-    }
-}
+domain_repo_set("search", $_SERVER["DOCUMENT_ROOT"] . "/search/app.zip");
