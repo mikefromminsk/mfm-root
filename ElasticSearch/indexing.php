40b80b1d59dfa6bef665edfcf455ebb2d6a39331
x29a100 <x29a100@mail.ru>
2020-11-11 14:59:26 +0300

    ElasticSearch

--- ElasticSearch/indexing.php
+++ ElasticSearch/indexing.php
@@ -8,11 +8,16 @@ $text = get_required("q");
 
 $response = array();
 
+function utf_split($word)
+{
+    return preg_split('//u', $word, null, PREG_SPLIT_NO_EMPTY);
+}
+
 function similar_distance($word1, $word2)
 {
     $distance = 0;
-    $chars1 = str_split($word1);
-    $chars2 = str_split($word2);
+    $chars1 = utf_split($word1);
+    $chars2 = utf_split($word2);
     $word1_length = sizeof($chars1);
     $word2_length = sizeof($chars2);
     $i = 0;
@@ -41,45 +46,61 @@ function similar_distance($word1, $word2)
     return $distance;
 }
 
-
 function indexing($word, $mode)
 {
-    $word_id = scalar("select word_id from words where word_text = '$word' and word_mode = '$mode'");
+    /*$word_id = scalar("select word_id from words where word_text = '$word' and word_mode = '$mode'");
     if ($word_id != null) {
         return select("select t2.*  from word_similar t1 left join words t2 on t1.word_id_similar == t2.word_id where word_id == $word_id");
-    } else {
-        $word_id = insertRowAndGetId("word", array(
-            "word_text" => $word,
-            "word_mode" => $mode,
-        ));
-        $result = [];
-        if ($mode == "text"){
-            $sort_chars = str_split($word);
-            sort($sort_chars);
-            $sort_word = join($sort_chars);
-            indexing($sort_word, "sort");
+    } else */ {
+        echo $mode;
 
-            $chars = count_chars($sort_word, 3);
-            indexing($chars, "chars");
+
+    $similar_words = select("select * from words where length(word_text) >= " . strlen($word) . " - 1 and length(word_text) <= " . strlen($word) . " + 1 and word_mode = '$mode'");
+
+    $word_id = insertRowAndGetId("words", array(
+        "word_text" => $word,
+        "word_mode" => $mode,
+    ));
+
+    $result = [];
+    echo json_encode($similar_words) . "\n";
+    foreach ($similar_words as $similar_word) {
+        $distance = similar_distance($word, $similar_word["word_text"]);
+        if ($distance <= 2) {
+            insertRow("word_similar", array(
+                "word_id" => $word_id,
+                "word_id_similar" => $similar_word["word_id"],
+            ));
+            insertRow("word_similar", array(
+                "word_id" => $similar_word["word_id"],
+                "word_id_similar" => $word_id,
+            ));
+            $result[] = $similar_word;
         }
-        $similar_words = select("select * from words where length(word_text) >= " . strlen($word) . " - 1 and length(word_text) <= " . strlen($word) . " + 1 and word_mode = '$mode'");
-        foreach ($similar_words as $similar_word)
-            if (similar_distance($word, $similar_word) <= 2){
-                insertRow("word_similar", array(
-                     "word_id" => $word_id,
-                     "word_id_similar" => $similar_word["word_id"],
-                ));
-                insertRow("word_similar", array(
-                     "word_id" => $similar_word["word_id"],
-                     "word_id_similar" => $word_id,
-                ));
-                $result[] = $similar_word;
-            }
-        return $similar_words;
     }
+
+    if ($mode == "text") {
+        $sort_chars = utf_split($word);
+        sort($sort_chars);
+        $sort_word = implode('', $sort_chars);
+        indexing($sort_word, "sort");
+
+        $chars = [];
+        foreach ($sort_chars as $sort_char)
+            if (array_search($sort_char, $chars) === false)
+                $chars[] = $sort_char;
+        $char_word = implode('', $chars);
+        indexing($char_word, "chars");
+    }
+
+    return $similar_words;
+}
 }
 
-indexing($text, "text");
+query("delete from words");
+query("delete from word_similar");
+indexing("привет", "text");
+indexing("привет2", "text");
 
 
 return $response;
--- ElasticSearch/search.php
+++ ElasticSearch/search.php
@@ -8,30 +8,34 @@ $text = get_required("q");
 
 $response = array();
 
+function utf_split($word)
+{
+    return preg_split('//u', $word, null, PREG_SPLIT_NO_EMPTY);
+}
+
 function search($word, $mode)
 {
     $word_id = scalar("select word_id from words where word_text = '$word' and word_mode = '$mode'");
     if ($word_id != null)
-        return select("select t2.*  from word_similar t1 left join words t2 on t1.word_id_similar == t2.word_id where word_id == $word_id");
+        return select("select t2.*  from word_similar t1 left join words t2 on t1.word_id_similar = t2.word_id where t1.word_id = $word_id");
     return null;
 }
 
 $text_search = search($text, "text");
 if ($text_search != null)
-    return json_encode($text_search);
+    die(json_encode($text_search));
 
-$sort_chars = str_split($text);
+$sort_chars = utf_split($text);
 sort($sort_chars);
 $sort_word = join($sort_chars);
 $sort_search = search($sort_word, "sort");
 if ($sort_search != null)
-    return json_encode($sort_search);
+    die(json_encode($sort_search));
 
 $chars = count_chars($sort_word, 3);
 $chars_search = search($chars, "chars");
 if ($text_search != null)
-    return json_encode($text_search);
+    die(json_encode($text_search));
 
-
-return $response;
+die(json_encode($response));
 
