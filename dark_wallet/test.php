171d8204f974a258b43626b317bf3dc9a7f9d727
x29a100 <x29a100@mail.ru>
2021-02-11 12:33:41 +0300

    dark_noe

--- dark_wallet/test.php
+++ dark_wallet/test.php
@@ -7,16 +7,16 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/dark_node/utils.php";
 // reg admin
 include_once $_SERVER["DOCUMENT_ROOT"] . "/dark_node/init.php";
 
-// generate pot
-// reg user
-// buy pots
-// save pots
+// generate pot coin_generate.php login admin
+// reg user reg.php
+// buy pots payment_start.php
+// save pots payment_finish.php
 
-// pay pot to friends
+// pay pot to friends coin_generate.php
 
-// generate tet local
-// send tet to fiends
+// generate tet local  coin_generate.php
+// send tet to fiends  coin_generate.php
 
-// reg receiver_user remote
-// user validate and download 10 tet local
-// remote_user upload 10 tet remote
+// reg receiver_user remote  reg.php
+// user validate and download 10 tet local wallet_download.php
+// remote_user upload 10 tet remote wallet_upload.php
