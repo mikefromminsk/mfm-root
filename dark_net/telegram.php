851a0de04b815f0a7104913b4041c6c7cecd3306
x29a100 <x29a100@mail.ru>
2021-03-09 17:35:12 +0300

    dark_net/telegram dark_net/mail

--- dark_net/telegram.php
+++ dark_net/telegram.php
@@ -1,1 +1,18 @@
 <?php
+
+
+function telegramSend($chatID, $message)
+{
+    file_get_contents("https://api.telegram.org/bot" . $GLOBALS["telegram_token"] . "/sendMessage?"
+        . http_build_query(array(
+            "text" => $message,
+            "chat_id" => $chatID,
+        )));
+}
+
+
+function telegramGetUpdates($offset = null)
+{
+    $response = file_get_contents("https://api.telegram.org/bot" . $GLOBALS["telegram_token"] . "/getUpdates?offset=$offset");
+    return json_decode($response, true);
+}
