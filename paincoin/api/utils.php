e3fb371d9933ee74e215e2875f40e23a3eb788a0
Mike <mike.haiduk@latoken.com>
2022-03-22 23:32:02 +0300

    text

--- paincoin/api/utils.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once "properties.php";
-include_once $_SERVER["DOCUMENT_ROOT"] . "/data/utils.php";
-include_once $_SERVER["DOCUMENT_ROOT"] . "/messenger/mail.php";
-include_once $_SERVER["DOCUMENT_ROOT"] . "/messenger/telegram.php";
