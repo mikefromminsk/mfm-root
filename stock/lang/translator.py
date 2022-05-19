82891bb3ffae8735861274c9319199032520d95d
Mike <mike.haiduk@latoken.com>
2022-05-20 00:34:16 +0300

    text

--- stock/lang/translator.py
+++ stock/lang/translator.py
@@ -2,6 +2,10 @@ import csv
 from lxml import etree
 import sys
 import os
+import requests
+
+response = requests.get('https://docs.google.com/spreadsheet/ccc?key=1zsouwYP3NAXHJlAB2lO0sqCc1DDT8Bl91qPTU2icQK8&output=csv')
+open("s.csv", "wb").write(response.content)
 
 reader = csv.reader(open("s.csv", encoding="utf8"))
 
@@ -17,7 +21,7 @@ for row in reader:
             strings.append("var str = {\n")
     else:
         for i in range(1, len(row)):
-            strings[i - 1] += row[0] + ": '" + row[i] + "',\n"
+            strings[i - 1] += row[0] + ": '" + (row[i] if row[i] else row[1]) + "',\n"
 
 for i in range(0, len(languages)):
     file = open(languages[i] + ".js", "w", encoding="utf8")
