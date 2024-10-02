39b8373e4dcc9b9dd523e4e865fbb870989b0ec6
Mike Haiduk <x29a100@gmail.com>
2024-10-02 13:39:46 +0300

    add package.json

--- telegram/api/utils.php
+++ telegram/api/utils.php
@@ -2,21 +2,23 @@
 include_once $_SERVER["DOCUMENT_ROOT"] . "/token/utils.php";
 include_once $_SERVER["DOCUMENT_ROOT"] . "/telegram/api/properties.php";
 
+$telegram_bot_apis = get_required(telegram_bot_apis);
+$bot = get_required(bot);
 
-function telegramSendToUsername($bot, $username, $text)
+$telegram_bot_api = $telegram_bot_apis[$bot];
+
+
+function telegramSend($bot, $username, $text)
 {
-    $telegram_bot_api = get_required(telegram_bot_apis)[$bot];
+    $telegram_bot_api = get_required(telegram_bot_api);
     return http_post("https://api.telegram.org/bot$telegram_bot_api/sendMessage", [
-        chat_id => dataGet([users, $username, $bot]),
+        chat_id => dataGet([telegram, users, $bot, $username]),
         text => $text,
     ]);
 }
 
 
-function telegramSendToAddress($bot, $address, $text)
+function telegramConnectAccountAndUser($address, $username, $domain)
 {
-    $username = dataGet([accounts, $address]);
-    if ($username) {
-        return telegramSendToUsername($bot, $username, $text);
-    }
+
 }
